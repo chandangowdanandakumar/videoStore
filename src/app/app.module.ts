2c7f3cd50e8166590b07215babca37e26012fc48
Chandan Gowda <chandangowdanandakumar@outlook.com>
2021-03-30 00:47:08 -0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignupComponent } from './home/signup/signup.component';
+import { LoginComponent } from './home/login/login.component';
+import { HomeRoutingModule } from './home/home-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
